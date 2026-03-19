f8ca4060b7e8a339edf2cbfa0975722f73bd92da
Sam Daramroei <31776791+cheerios-design@users.noreply.github.com>
2026-03-20 01:50:11 +0300

    fix: update deleteInvoice function to return void and handle errors properly; adjust TypeScript config deprecation setting

--- w02_learn/nextjs-dashboard/app/lib/actions.ts
+++ w02_learn/nextjs-dashboard/app/lib/actions.ts
@@ -103,13 +103,13 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
-export async function deleteInvoice(id: string) {
+export async function deleteInvoice(id: string): Promise<void> {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    console.error(error);
+    throw new Error('Database Error: Failed to Delete Invoice.');
   }
 }
 
